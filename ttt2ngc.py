42bedc19a9a15ea66327a58c22c488cf8ae870d1
Anders Wallin <anders.e.e.wallin@gmail.com>
2012-03-12 14:37:02 +0200

    show how to change font

--- ttt2ngc.py
+++ ttt2ngc.py
@@ -9,6 +9,21 @@ def ttt2ngc(text,scale):
     wr.cubic = False   # if False, approximate cubic with arc/line 
     
     wr.scale = float(1)/float(scale)
+    # set the font
+    wr.setFont(0) # FreeSerif
+    #wr.setFont(1) # FreeSerifBold
+    #wr.setFont(2) # FreeSerifItalic
+    #wr.setFont(3) # FreeSerifBoldItalic
+    #wr.setFont(4) # FreeMono
+    #wr.setFont(5) # FreeMonoBold
+    #wr.setFont(6) # FreeMonoBoldOblique
+    #wr.setFont(7) # FreeMonoOblique
+    #wr.setFont(8) # FreeSans
+    #wr.setFont(9) # FreeSansBold
+    #wr.setFont(10) # FreeSansBoldOblique
+    #wr.setFont(11) # FreeSansOblique
+
+    
     return ttt.ttt(text,wr)
 
 if __name__ == "__main__":  
